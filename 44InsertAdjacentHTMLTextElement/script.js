df90359fa1c174041d5ebeb7dba1ad394944659a
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-05 22:16:37 +0600

    learned insertAdjacent HTML/text/element methods

---
+++ 44InsertAdjacentHTMLTextElement/script.js
@@ -0,0 +1,5 @@
+first.insertAdjacentHTML('beforebegin', '<div class="text">beforebegin</div>')
+first.insertAdjacentHTML('beforeend', '<div class="text">beforeend</div>')
+first.insertAdjacentHTML('afterbegin', '<div class="text">afterbegin</div >')
+first.insertAdjacentHTML('afterend','<div class="text">afterend</div>')
+first.remove()
