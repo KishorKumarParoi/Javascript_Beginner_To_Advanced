ddb57996a98cac42b567998fb4853097606241fd
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-05 22:17:29 +0600

    learned className and classList and their operation

---
+++ 45classnameClasslist/script.js
@@ -0,0 +1,11 @@
+first.className = "yellow text-dark"
+first.classList
+first.classList.toggle('red')
+first.classList.toggle('yellow') // lagi hei to hotta do nehi hei to laga do
+first.classList.add('text-dark')
+first.classList.contains('red')
+first.classList.toggle('red')
+// first.classList.toggle('yellow text-dark')
+first.classList.toggle('yellow')
+first.classList.toggle('text-dark')
+first.classList.add('text-dark')
