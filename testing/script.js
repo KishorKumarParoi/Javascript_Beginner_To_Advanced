3234c09706c29dbf3ce014850deb237af9712aeb
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-12 19:44:37 +0600

    testing js file

---
+++ testing/script.js
@@ -0,0 +1,21 @@
+function Display(some) {
+  document.getElementById('test').innerHTML = some;
+}
+function hello() {
+  // console.log("hello World")
+  Display("Hello World")
+}
+
+function goodmorning() {
+  // console.log("good morning")
+  Display("Good Morning")
+}
+
+function calculator(let aa, let bb){
+  return aa + ab;
+}
+
+hello()
+goodmorning()
+
+Display(calculator(4,5))
