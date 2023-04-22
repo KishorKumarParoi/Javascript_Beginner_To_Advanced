5dd4475ea6403e54cf531915dffdf94e0f1cdae9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-22 23:28:56 +0600

    learned try/catch and excaping error in promise

---
+++ 62-Try-Catch/script.js
@@ -0,0 +1,28 @@
+setTimeout(() => {
+  console.log("Hacking Wifi and Password ..... Please Wait")
+}, 10)
+
+// console.log(Rahul)  // don't run 
+
+try{
+  // setTimeout(()=>{   schedule code throws error
+  //   console.log(rahul)
+  // },100)
+  console.log(rahul)
+}
+catch(error){
+  console.log("balle Balle error vai")
+  console.log(error)
+}
+
+setTimeout(() => {
+  console.log("Fetching Wifi and Password ..... Please Wait")
+}, 3000)
+
+setTimeout(() => {
+  console.log("Gotcha Wifi and Password ..... ")
+}, 5000)
+
+setTimeout(() => {
+  console.log("Hacked Rahul's Wifi and Password .....")
+}, 7000)
