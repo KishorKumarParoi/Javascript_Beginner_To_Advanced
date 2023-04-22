1bef30fd1219fbf43ebb7c1cf118d544c67c454b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-23 00:01:31 +0600

    learned mighty finally

---
+++ 64-Finally/script.js
@@ -0,0 +1,28 @@
+const f = () => {
+
+  try {
+    let a = 12;
+    // console.log(program)
+    console.log("Program ran successfully")
+    return
+  }
+  catch (error) {
+    console.log("This is an Error")
+    console.log(p)
+  }
+
+  // finally be like "mujhe chalte rahena hei"
+
+  finally {      // code will always run like modiji
+    console.log("I am a good coder")
+    // Close the file
+    // Exit the Loop
+    // Break the chain
+    // Write to the log file
+    // finally does cleanup
+  }
+}
+
+f()
+
+console.log("End")
