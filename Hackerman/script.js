fdef665e1db2f250dc3bc3f8d99e459efca73b77
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-24 01:01:29 +0600

    funny project hackerman

---
+++ Hackerman/script.js
@@ -0,0 +1,31 @@
+setInterval(() => {
+  setTimeout(() => {
+    console.log("Initialising hack program ......")
+    hacker.innnerHTML = "Initialising hack program ......"
+  }, 2000)
+
+  setTimeout(() => {
+    console.log("Hacking Pallabi Karmaker Instagram ID ......")
+    hacker.innerHTML = "Hacking Pallabi Karmaker Instagram ID ......"
+  }, 4000)
+
+  setTimeout(() => {
+    console.log("Fetching username Instagram ID ......")
+    hacker.innerHTML = "Fetching username Instagram ID ......"
+  }, 6000)
+  setTimeout(() => {
+    console.log("Connecting to Instagram ......")
+    hacker.innerHTML = "Connecting to Instagram ......"
+  }, 8000)
+
+  setTimeout(() => {
+    console.log("Logging in to Pallabi Karmaker's Account ......")
+    hacker.innerHTML = "Logging in to Pallabi Karmaker's Account ......"
+  }, 10000)
+
+
+  setTimeout(() => {
+    console.log("Hurrayy, Hacking Succcessfull ......")
+    hacker.innerHTML = "Hurray, Hacked Your Instagram Account and Password ......"
+  }, 11000)
+}, 1000)
